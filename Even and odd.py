c6c76c7ee6c173ce8d476aa95d8b6120eb8d2838
kanikavishwakarma <85575249+kanikavishwakarma@users.noreply.github.com>
2022-05-08 11:17:46 +0530

    Add files via upload

---
+++ Even and odd.py
@@ -0,0 +1,24 @@
+print("Enter any number:")
+num = int(input())
+print("rem:",num%2)
+
+if num == 0:
+    print("Number is zero")
+elif num % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
+
+#  FOR FLOATS AS WELL
+
+print("Enter any number:")
+num = float(input())
+print("rem:",num%2)
+
+if num == 0:
+    print("Number is zero")
+elif num % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
