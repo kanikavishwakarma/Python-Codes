7ee894462d0713259c5bd0b1900a3e5e6a8b9046
kanikavishwakarma <85575249+kanikavishwakarma@users.noreply.github.com>
2022-05-08 11:25:42 +0530

    Add files via upload

---
+++ Length of a list.py
@@ -0,0 +1,41 @@
+#  ********************* Method 1 ************************
+
+list1 = [1,2,3,4,5]
+print("The first list is : ",list1)
+
+counter = 0
+for i in list1:
+    counter += 1
+
+print("Length of list1 is : ",counter)
+print()
+
+
+#  ********************* Method 2 ************************
+
+list2 = []
+list2.append("Kanika")
+list2.append("Anshima")
+list2.append("Anushka")
+list2.append("Harsh")
+
+print("The second list is : ",list2)
+
+print("The length of the list2 is : ",len(list2))
+
+l = len(list2)
+print("The length of list2 is : ",l)
+print()
+
+
+# ********************* METHOD 3 ************************
+
+list3 = [1,2,3,4,5,6,7,8,9,10]
+print("The third list is : " , str(list3))
+
+counter = 0
+for i in list3:
+    counter += 1
+
+print("Length of list3 is : " , str(counter))
+
