f42bf14139afa9fd2ca1fcd6459b41fd691b5f76
kanikavishwakarma <85575249+kanikavishwakarma@users.noreply.github.com>
2022-05-09 00:32:35 +0530

    Add files via upload

---
+++ name = main concept.py
@@ -0,0 +1,16 @@
+# ******************************** _NAME_ == '_MAIN_' ***************************
+
+def printhar(string):
+    return f"harry is a good {string}"
+
+def add(num1,num2):
+    return  num1+num2
+
+print("name is",__name__)
+
+if __name__=='__main__':
+    print(printhar("boy"))
+    sum=add(4,5)
+    print("Sum=",sum)
+
+  
