91581abfb7c8cc865e5c61bc4060decb65994c74
kanikavishwakarma <85575249+kanikavishwakarma@users.noreply.github.com>
2022-05-08 11:26:03 +0530

    Add files via upload

---
+++ Length of string.py
@@ -0,0 +1,14 @@
+
+string = "My name is Kanika Vishwakarma"
+length = len(string)
+
+print("Length of string : ",length)
+print("Length of string : "+str(length))
+print("Length of string : {}".format(length))
+
+for i in range(length):
+    print(i)
+
+
+str = "12345"
+print("Length of string : ",len(str))   
